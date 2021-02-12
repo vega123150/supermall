a211850f6c64311209272ceecf87ecfb9be641f2
HWJ <1320473865@qq.com>
2021-02-12 20:19:43 +0800

    '初始化项目'

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+
+Vue.use(VueRouter)
+
+const originalReplace = VueRouter.prototype.replace
+// 重写了原型上的push方法，统一的处理了错误信息
+VueRouter.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err)
+}
+
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+})
+
+export default router
